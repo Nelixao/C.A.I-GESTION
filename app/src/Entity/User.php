09cba1a8f4c15e91d64ba41b8b779b0c61af8fde
nelixao <ncelisalonso@gmail.com>
2025-07-25 22:16:06 -0600

    docker-correcto

---
+++ app/src/Entity/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 55)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $contrasena = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getContrasena(): ?string
+    {
+        return $this->contrasena;
+    }
+
+    public function setContrasena(string $contrasena): static
+    {
+        $this->contrasena = $contrasena;
+
+        return $this;
+    }
+}
