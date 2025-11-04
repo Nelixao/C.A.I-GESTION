c9f5d413f8f478e40baf531d60c8c6cded03061d
Nelixao <ncelisalonso@gmail.com>
2025-11-04 15:42:41 -0600

    Resuelve conflictos tras merge main->dev

--- app/migrations/Version20251016163650.php
+++ app/migrations/Version20251016163650.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250726163119 extends AbstractMigration
+final class Version20251016163650 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,12 @@ final class Version20250726163119 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP nombre');
-        $this->addSql('ALTER TABLE user RENAME INDEX email TO UNIQ_8D93D649E7927C74');
-        $this->addSql('ALTER TABLE user RENAME INDEX idx_8d93d64988987678 TO IDX_8D93D649D60322AC');
+        $this->addSql('ALTER TABLE oficio DROP file_name');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD nombre VARCHAR(55) NOT NULL');
-        $this->addSql('ALTER TABLE user RENAME INDEX uniq_8d93d649e7927c74 TO email');
-        $this->addSql('ALTER TABLE user RENAME INDEX idx_8d93d649d60322ac TO IDX_8D93D64988987678');
+        $this->addSql('ALTER TABLE oficio ADD file_name VARCHAR(255) DEFAULT NULL');
     }
 }
